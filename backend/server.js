10d4468b7d9f8cc7ed57946e12d7af204c584120
vandraaa <kevinandranugroho732@gmail.com>
2024-11-01 21:02:12 +0700

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database.js';
+import cors from 'cors';
 import routes from './routes/web.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 const port = process.env.PORT;
 
