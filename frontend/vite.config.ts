2508221c8175a8039a90ee9e4c6ff065ea5fb71b
vandraaa <kevinandranugroho732@gmail.com>
2025-01-10 21:23:55 +0700

    deployment frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/'
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
 })
